2a98494a7d9fa6c1b70e286495ae4c6654823ca6
Kevin Robb <kevin.robb@ou.edu>
2020-02-04 09:34:48 -0600

    Create solution.cpp

---
+++ 6 - Old McDonald Had Some Glorps/solution.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+    double xVal[N];
+    double yVal[N];
+    for (int i=0; i<N; i++) {
+        cin >> xVal[i];
+        cin >> yVal[i];
+    }
+    //calculation time babey
+    bool flag = false;
+    for (int r=0; r<N; r++) {
+        for (int c=0; c<N; c++) {
+            if (r == c) 
+                continue;
+            if (powf(xVal[r] - xVal[c], 2.0) + powf(yVal[r] - yVal[c], 2.0) <= 1.0) {
+                flag = true;
+                break;
+            }
+        }
+    }
+    if (flag)
+        cout << "Unsafe!";
+    else
+        cout << "Safe!";
+    return 0;
+}
