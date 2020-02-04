371914861872e0e94d917ce198b9ca23dc8ad915
Kevin Robb <kevin.robb@ou.edu>
2020-02-04 09:19:07 -0600

    Create solution.java

---
+++ 2 - Program Discipline/solution.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        String s=scan.nextLine();
+        System.out.println("Did"+s.substring(2));
+    }
+}
