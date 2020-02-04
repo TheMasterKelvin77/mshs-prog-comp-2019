9539c9d6a287ba22fbde0c45d40be375b5107880
Kevin Robb <kevin.robb@ou.edu>
2020-02-04 09:29:48 -0600

    Create solution.cpp

---
+++ 5 - Roll for Initiative/solution.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    int die;
+    double sum;
+    for (int i=0; i<N; i++) {
+        cin >> die;
+        sum += (die + 1.0) / 2.0;
+    }
+    printf("%.3f", sum / N);
+    return 0;
+}
